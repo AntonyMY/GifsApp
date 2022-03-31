319e89a5549cccfa9b99571c8ecb6295fe08de25
AntonyDev <antonymyprog@gmail.com>
2022-03-31 00:13:48 -0500

    HtpParams y Animate sTyles.css

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Gif, SearchGifsResponse } from '../interface/gifs.interface';
 
@@ -7,8 +7,9 @@ import { Gif, SearchGifsResponse } from '../interface/gifs.interface';
 })
 export class GifsService {
 
-  private apikey    : string   = '98pssTvGsaq4OpHpdKRGHGFJXwvw5Jk5';
-  private _historial: string[] = [];
+  private apikey     : string   = '98pssTvGsaq4OpHpdKRGHGFJXwvw5Jk5';
+  private servicioUrl: string   = 'https://api.giphy.com/v1/gifs';
+  private _historial : string[] = [];
 
   //TODO: Cambiar any por su tipo correspondiente
   public resultados : Gif[]    = [];
@@ -39,10 +40,17 @@ export class GifsService {
       localStorage.setItem('historial', JSON.stringify(this._historial) );
     }
 
+    const params = new HttpParams()
+        .set('api_key', this.apikey)
+        .set('limit','10')
+        .set('q', query);
+
+    console.log(params.toString());
+
     //El módulo HTTP usa observables para manejar la peticiones y respuestas AJAX.
     //Un observable en angular es  modo de implementación de la programación reactiva, que básicamente pone en funcionamiento diversos actores para producir los efectos deseados, que es reaccionar ante el flujo de los distintos eventos producidos. Mejor dicho, producir dichos eventos y consumirlos de diversos modos.
     //EL subscribe se ejecuta cuando tenemos la resolución del get [del link http del get]
-    this.http.get<SearchGifsResponse>(`https://api.giphy.com/v1/gifs/search?api_key=98pssTvGsaq4OpHpdKRGHGFJXwvw5Jk5&q=${ query  }&limit=10`)
+    this.http.get<SearchGifsResponse>(`${ this.servicioUrl }/search`, { params })
       .subscribe( ( resp: any ) => {
         console.log( resp.data );
         this.resultados = resp.data;
